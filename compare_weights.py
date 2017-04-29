f6b1a07b9c2c5e60aef93b3c230092a813d69e1a
Richard Horton <ezchx@users.noreply.github.com>
2017-04-29 10:20:14 -0500

    Update compare_weights.py

--- compare_weights.py
+++ compare_weights.py
@@ -1,7 +1,7 @@
 def compare_weights(model_1, model_2):
 
-    same = True
     for i in range(len(model_1.layers)):
+        same = True
         for j in range(len(model_1.layers[i].get_weights())):
             if not (np.array_equal(model_1.layers[i].get_weights()[j], model_2.layers[i].get_weights()[j])):
                 same = False
